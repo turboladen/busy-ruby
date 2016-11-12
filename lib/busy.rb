03bacb6430f66790eea732fe91462fa7d3a5170b
Steve Loveless <steve.loveless@gmail.com>
2016-11-12 11:45:32 -0800

    Fiddle wrapper

--- lib/busy.rb
+++ lib/busy.rb
@@ -1,5 +1,12 @@
-require "busy/version"
+require_relative 'busy/version'
+require 'fiddle'
+require 'rack'
+require 'rack/builder'
 
 module Busy
   # Your code goes here...
 end
+
+library = Fiddle::dlopen('target/debug/libbusy.dylib')
+
+Fiddle::Function.new(library['initialize_busy'], [], Fiddle::TYPE_VOIDP).call
