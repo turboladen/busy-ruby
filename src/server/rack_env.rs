584b4378b0581a914d51f6d6dc1e69126453bacb
Steve Loveless <steve.loveless@gmail.com>
2016-11-12 22:48:31 -0800

    Better request handling
    Actually set rack env values parsed by hyper. Still more to do here.
    
    Also, still need to implement `rack.input`.

--- src/server/rack_env.rs
+++ src/server/rack_env.rs
@@ -1,10 +1,8 @@
 use hyper::header::{ContentLength, Headers};
-// use hyper::http::h1::{HttpWriter};
 use hyper::server::{Request, Response};
 use hyper::status::StatusCode;
 use hyper::uri::RequestUri;
 use ruru::{Class};
-// use hyper::version::HttpVersion;
 
 use ruru::{Array, Boolean, Fixnum, Hash, Object, RString};
 
@@ -15,38 +13,48 @@ pub struct RackEnv {
 impl<'a, 'b> From<Request<'a, 'b>> for RackEnv {
     fn from(req: Request) -> Self {
         let mut env = Hash::new();
+        env.store(RString::new("SERVER_SOFTWARE"), RString::new("busy bruh"));
 
+        // TODO: handle relative paths
         if let RequestUri::AbsolutePath(uri) = req.uri {
             let uri = uri.as_str();
             println!("URI: {}", uri);
 
+            // TODO: Not sure if these should all be the same
             env.store(RString::new("PATH_INFO"), RString::new(uri));
             env.store(RString::new("REQUEST_PATH"), RString::new(uri));
             env.store(RString::new("REQUEST_URI"), RString::new(uri));
         };
 
-        env.store(RString::new("HTTP_HOST"), RString::new("localhost:8080"));
-        env.store(RString::new("REMOTE_ADDR"), RString::new("127.0.0.1"));
-        env.store(RString::new("REQUEST_METHOD"), RString::new("GET"));
+        let remote_addr = format!("{}", req.remote_addr.ip());
+        env.store(RString::new("REMOTE_ADDR"), RString::new(remote_addr.as_str()));
+
+        let request_method = format!("{}", req.method);
+        env.store(RString::new("REQUEST_METHOD"), RString::new(request_method.as_str()));
+
+        let http_version = format!("{}", req.version);
+        env.store(RString::new("HTTP_VERSION"), RString::new(http_version.as_str()));
+        env.store(RString::new("SERVER_PROTOCOL"), RString::new(http_version.as_str()));
+
         env.store(RString::new("SERVER_NAME"), RString::new("localhost"));
         env.store(RString::new("SERVER_PORT"), RString::new("8080"));
 
-        // let http_version = format!("{}", req.version);
-        // env.store(RString::new("HTTP_VERSION"), RString::new(http_version.as_str()));
-        // env.store(RString::new("SERVER_PROTOCOL"), RString::new(http_version.as_str()));
-        env.store(RString::new("HTTP_VERSION"), RString::new("HTTP/1.1"));
-        env.store(RString::new("SERVER_PROTOCOL"), RString::new("HTTP/1.1"));
-        env.store(RString::new("SERVER_SOFTWARE"), RString::new("busy bruh"));
+        //---------------------------------------------------------------------
+        // Headers
+        for header in req.headers.iter() {
+            let name = format!("HTTP_{}", header.name().to_uppercase());
+            let value = header.value_string();
+            env.store(RString::new(name.as_str()), RString::new(value.as_str()));
+        }
 
+        //---------------------------------------------------------------------
+        // Rack-specific
         env.store(RString::new("rack.input"), RString::new(""));
         env.store(RString::new("rack.multiprocess"), Boolean::new(false));
         env.store(RString::new("rack.thread"), Boolean::new(true));
         env.store(RString::new("rack.url_scheme"), RString::new("http"));
 
-        // let rack_version = Class::from_existing("Rack").const_get("VERSION");
-        let mut rack_version = Array::with_capacity(2);
-        rack_version.push(Fixnum::new(1));
-        rack_version.push(Fixnum::new(3));
+        let rack_version = Class::from_existing("Rack").const_get("VERSION");
         env.store(RString::new("rack.version"), rack_version);
 
         RackEnv { env: env }
